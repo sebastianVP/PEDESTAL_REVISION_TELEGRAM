c2cb7e5f240e05c02c79f1c002eb848007eccd95
sebastianVP <alex.valdezp22@gmail.com>
2025-01-27 17:23:23 -0500

    update

--- diccionario.py
+++ diccionario.py
@@ -2,28 +2,38 @@ import os
 import time
 
 # Ruta del directorio
-directorio = "/DATA_RM/DATA/HYO@2025-01-27T16-14-33/position"
+directorio_principal = "/DATA_RM/DATA/HYO@2025-01-27T16-14-33/position"
 
-# Obtener la lista de archivos en el directorio
-archivos = os.listdir(directorio)
+# Obtener la lista de directorios dentro del directorio principal
+directorios = [d for d in os.listdir(directorio_principal) if os.path.isdir(os.path.join(directorio_principal, d))]
 
-# Filtrar solo archivos (excluyendo directorios)
-archivos = [archivo for archivo in archivos if os.path.isfile(os.path.join(directorio, archivo))]
+# Ordenar los directorios por fecha de creación (el más reciente primero)
+directorios.sort(key=lambda x: os.path.getctime(os.path.join(directorio_principal, x)), reverse=True)
 
-# Ordenar los archivos por fecha de modificación (los últimos modificados primero)
-archivos.sort(key=lambda x: os.path.getmtime(os.path.join(directorio, x)), reverse=True)
-print(archivos)
-# Listar los últimos 3 archivos
-ultimos_3_archivos = archivos[:3]
 
-# Obtener la fecha de creación del directorio
-fecha_creacion_directorio = os.path.getctime(directorio)
-fecha_creacion_directorio = time.ctime(fecha_creacion_directorio)
+# Seleccionar el último directorio creado
+if directorios:
+    ultimo_directorio = directorios[0]
+    ruta_ultimo_directorio = os.path.join(directorio_principal, ultimo_directorio)
 
-# Mostrar los resultados
-print("Últimos 3 archivos:")
-for archivo in ultimos_3_archivos:
-    print(archivo)
+    # Obtener la lista de archivos en el último directorio
+    archivos = [archivo for archivo in os.listdir(ruta_ultimo_directorio) if os.path.isfile(os.path.join(ruta_ultimo_directorio, archivo))]    
+
+
+    # Ordenar los archivos por fecha de modificación (los últimos modificados primero)
+    archivos.sort(key=lambda x: os.path.getmtime(os.path.join(ruta_ultimo_directorio, x)), reverse=True)
+    print(archivos)
+    # Listar los últimos 3 archivos
+    ultimos_3_archivos = archivos[:3]
+
+    # Obtener la fecha de creación del directorio
+    fecha_creacion_directorio = os.path.getctime(directorio)
+    fecha_creacion_directorio = time.ctime(fecha_creacion_directorio)
+
+    # Mostrar los resultados
+    print("Últimos 3 archivos:")
+    for archivo in ultimos_3_archivos:
+        print(archivo)
 
 print("\nFecha de creación del directorio:")
 print(fecha_creacion_directorio)
