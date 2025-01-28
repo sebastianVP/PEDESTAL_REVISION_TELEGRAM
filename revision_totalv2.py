dcce68e5c8f2b5e60a55dde7b35c42fd3d452879
sebastianVP <alex.valdezp22@gmail.com>
2025-01-28 11:23:24 -0500

    diferencia de tiempo

--- revision_totalv2.py
+++ revision_totalv2.py
@@ -181,9 +181,11 @@ def generate_pdf_report(data,output_file):
         # Momento del último archivo guardado
         tiempo_actual = datetime.now()
         ultimo_archivo = archivos[0]
-        print("Tiempo actual",tiempo_actual)
-        print("Ultimo archivo",ultimo_archivo)
-        diferencia_tiempo = tiempo_actual - ultimo_archivo["fecha_creacion"]
+        if tiempo_actual> ultimo_archivo["fecha_creacion"]:
+            diferencia_tiempo = tiempo_actual - ultimo_archivo["fecha_creacion"]
+        else:
+            diferencia_tiempo = ultimo_archivo["fecha_creacion"]-tiempo_actual
+
         story.append(Spacer(1, 12))
         story.append(Paragraph(
             f"Tiempo transcurrido desde el último archivo guardado: {diferencia_tiempo.seconds} segundos.",
