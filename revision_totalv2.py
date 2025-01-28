c9ef3637f9ccee11161faa5e8f8c741ffbe96976
sebastianVP <alex.valdezp22@gmail.com>
2025-01-28 09:40:21 -0500

    update2

--- revision_totalv2.py
+++ revision_totalv2.py
@@ -8,7 +8,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import SimpleDocTemplate,Paragraph,Spacer
 from datetime import datetime
-
+import datetime
 
 def execute_command(command):
     try:
